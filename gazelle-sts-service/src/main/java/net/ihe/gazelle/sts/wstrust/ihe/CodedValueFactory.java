18ada8c4dade2ab399753ca2fbcae6d2af462163
stl-steve-moore <stl.steve.moore@gmail.com>
2023-12-19 19:55:48 -0600

    Add Purpose of Use codes for RCE 1.1 testing.

--- gazelle-sts-service/src/main/java/net/ihe/gazelle/sts/wstrust/ihe/CodedValueFactory.java
+++ gazelle-sts-service/src/main/java/net/ihe/gazelle/sts/wstrust/ihe/CodedValueFactory.java
@@ -92,14 +92,28 @@ public class CodedValueFactory {
             codedValueMap.put("REQUESTOID",      new CodedValue("REQUEST",     "REQUEST",      "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Individual Access Services"));
             codedValueMap.put("COVERAGEOID",     new CodedValue("COVERAGE",    "COVERAGE",     "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Government Benefits Determination"));
 
+
+            // Code value is OK, but the coding system UID is wrong.
+            // Added 2023.12.13 to support QTF 1.1 negative testing
+            codedValueMap.put("T-TRTMNT-OID",    new CodedValue("T-TRTMNT",   "T-TRTMNT",  "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Treatment"));
+            codedValueMap.put("T-PYMNT-OID",     new CodedValue("T-PYMNT",    "T-PYMNT",   "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Payment"));
+            codedValueMap.put("T-HCO-OID",       new CodedValue("T-HCO",      "T-HCO",     "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Health Care Operations"));
+            codedValueMap.put("T-PH-OID",        new CodedValue("T-PH",       "T-PH",      "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Public Health"));
+            codedValueMap.put("T-IAS-OID",       new CodedValue("T-IAS",      "T-IAS",     "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Individual Access Services"));
+            codedValueMap.put("T-GOVDTRM-OID",   new CodedValue("T-GOVDTRM",  "T-GOVDTRM", "2.16.840.1.113883.3.7204.1.5.2.199","QHIN Exchange Purpose","Government Benefits Determination"));
+            // End 2023.12.13
+
             // Correct coding system, but a code that does not exist
             codedValueMap.put("REASSURANCE",      new CodedValue("REASSURANCE",      "REASSURANCE", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Text"));
             // TREATMENT code from NHIN coding system
-            codedValueMap.put("LEGACYTREATMENT",  new CodedValue("LEGACYTREATMENT", "TREATMENT",  "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Legacy NHIN POU Treatment"));
+            codedValueMap.put("LEGACYTREATMENT",  new CodedValue("LEGACYTREATMENT",  "TREATMENT",   "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Legacy NHIN POU Treatment"));
 
             // This is a case where the proper coded value is used. There are other parts of the SAML assertions
             // that are tweaked to generate an error condition.
-            codedValueMap.put("REQUESTATTRS",     new CodedValue("REQUESTATTRS",     "REQUEST",      "2.16.840.1.113883.3.7204.1.5.2.1","RCE-purpose","Individual Access Services"));
+            codedValueMap.put("REQUESTATTRS",     new CodedValue("REQUESTATTRS",   "REQUEST",    "2.16.840.1.113883.3.7204.1.5.2.1","RCE-purpose","Individual Access Services"));
+            // Added 2023.12.13 to support QTF 1.1 negative testing
+            codedValueMap.put("T-IAS-ATTRS",     new CodedValue("T-IAS-ATTRS",     "T-IAS",      "2.16.840.1.113883.3.7204.1.5.2.1","RCE-purpose","Individual Access Services"));
+            // End 2023.12.13
         }
     }
 }
