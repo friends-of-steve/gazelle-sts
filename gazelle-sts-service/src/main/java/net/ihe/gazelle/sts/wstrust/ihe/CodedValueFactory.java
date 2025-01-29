76ad728e2708cf5ff7586f13f9813d3cab7ece89
stl-steve-moore <stl.steve.moore@gmail.com>
2025-01-28 19:27:29 -0600

    Remove the java code that previously had some of the coded values for specific Sequoia projects. The java code was commented out, but we removed it with this commit to clean up the files. These values belong in the runtime configuration files and not in source code.

--- gazelle-sts-service/src/main/java/net/ihe/gazelle/sts/wstrust/ihe/CodedValueFactory.java
+++ gazelle-sts-service/src/main/java/net/ihe/gazelle/sts/wstrust/ihe/CodedValueFactory.java
@@ -35,12 +35,6 @@ public class CodedValueFactory {
 
     }
 
-    /*
-    public CodedValueFactory(HashMap<String, CodedValue> codedValueMap) {
-        this.codedValueMap = codedValueMap;
-    }
-     */
-
     public CodedValue getCodedValue(String id) {
         //System.out.println("Get Coded Value: " + id);
         LOG.debug("CodedValueFactory::getCodedValue key = " + id);
@@ -65,7 +59,6 @@ public class CodedValueFactory {
         } else {
             return false;
         }
-
     }
 
     private void populateInboundSupportedCodedValues() {
@@ -83,28 +76,6 @@ public class CodedValueFactory {
             } else {
                 LOG.error("ERROR: Did not find or read /opt/sts/inboundSupportedCodes.xml. Only default inbound code values will be supported");
                 inboundSupportedCodedValues = new HashSet<>();
-
-/*                inboundSupportedCodedValues.add(   "TREATMENT:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(     "PAYMENT:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(  "OPERATIONS:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add("PUBLICHEALTH:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(     "REQUEST:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(    "COVERAGE:2.16.840.1.113883.3.7204.1.5.2.1");
-
-                // Added 2023.11.20 to support QHIN 1.1
-                inboundSupportedCodedValues.add(  "T-TRTMNT:.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(  "T-PYMNT:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(    "T-HCO:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(     "T-PH:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add(    "T-IAS:2.16.840.1.113883.3.7204.1.5.2.1");
-                inboundSupportedCodedValues.add("T-GOVDTRM:2.16.840.1.113883.3.7204.1.5.2.1");
-                // End Add 2023.11.20
-
-                // Added 2024.09.28 to support eHx ACP
-                inboundSupportedCodedValues.add(       "COVERAGE:2.16.840.1.113883.3.18.7.1");
-                // End Add 2024.09.28
-
- */
             }
         }
     }
@@ -117,63 +88,6 @@ public class CodedValueFactory {
                 LOG.warn("ERROR: Did not find or read /opt/sts/allCodes.xml. Only default code values for output will be supported");
 
                 allCodes = new HashMap<>();
-/*
-                allCodes.put("TREATMENT", new CodedValue("TREATMENT", "TREATMENT", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Treatment"));
-                allCodes.put("PAYMENT", new CodedValue("PAYMENT", "PAYMENT", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Payment"));
-                allCodes.put("OPERATIONS", new CodedValue("OPERATIONS", "OPERATIONS", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Health Care Operations"));
-                allCodes.put("PUBLICHEALTH", new CodedValue("PUBLICHEALTH", "PUBLICHEALTH", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Public Health"));
-                allCodes.put("REQUEST", new CodedValue("REQUEST", "REQUEST", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Individual Access Services"));
-                allCodes.put("COVERAGE", new CodedValue("COVERAGE", "COVERAGE", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Government Benefits Determination"));
-
-                // Added 2023.11.20 to support QHIN 1.1
-                allCodes.put("T-TRTMNT", new CodedValue("T-TRTMNT", "T-TRTMNT", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Treatment"));
-                allCodes.put("T-PYMNT", new CodedValue("T-PYMNT", "T-PYMNT", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Payment"));
-                allCodes.put("T-HCO", new CodedValue("T-HCO", "T-HCO", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Health Care Operations"));
-                allCodes.put("T-PH", new CodedValue("T-PH", "T-PH", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Public Health"));
-                allCodes.put("T-IAS", new CodedValue("T-IAS", "T-IAS", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Individual Access Services"));
-                allCodes.put("T-GOVDTRM", new CodedValue("T-GOVDTRM", "T-GOVDTRM", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Government Benefits Determination"));
-                // End Add 2023.11.20
-
-                // Code value is OK, but the coding system UID is wrong.
-                allCodes.put("TREATMENTOID", new CodedValue("TREATMENT", "TREATMENT", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Treatment"));
-                allCodes.put("PAYMENTOID", new CodedValue("PAYMENT", "PAYMENT", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Payment"));
-                allCodes.put("OPERATIONSOID", new CodedValue("OPERATIONS", "OPERATIONS", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Health Care Operations"));
-                allCodes.put("PUBLICHEALTHOID", new CodedValue("PUBLICHEALTH", "PUBLICHEALTH", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Public Health"));
-                allCodes.put("REQUESTOID", new CodedValue("REQUEST", "REQUEST", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Individual Access Services"));
-                allCodes.put("COVERAGEOID", new CodedValue("COVERAGE", "COVERAGE", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Government Benefits Determination"));
-
-                // Code value is OK, but the coding system UID is wrong.
-                // Added 2023.12.13 to support QTF 1.1 negative testing
-                allCodes.put("T-TRTMNT-OID", new CodedValue("T-TRTMNT", "T-TRTMNT", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Treatment"));
-                allCodes.put("T-PYMNT-OID", new CodedValue("T-PYMNT", "T-PYMNT", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Payment"));
-                allCodes.put("T-HCO-OID", new CodedValue("T-HCO", "T-HCO", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Health Care Operations"));
-                allCodes.put("T-PH-OID", new CodedValue("T-PH", "T-PH", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Public Health"));
-                allCodes.put("T-IAS-OID", new CodedValue("T-IAS", "T-IAS", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Individual Access Services"));
-                allCodes.put("T-GOVDTRM-OID", new CodedValue("T-GOVDTRM", "T-GOVDTRM", "2.16.840.1.113883.3.7204.1.5.2.199", "QHIN Exchange Purpose", "Government Benefits Determination"));
-                // End 2023.12.13
-
-                // Correct coding system, but a code that does not exist
-                allCodes.put("REASSURANCE", new CodedValue("REASSURANCE", "REASSURANCE", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Text"));
-                // TREATMENT code from NHIN coding system
-                allCodes.put("LEGACYTREATMENT", new CodedValue("LEGACYTREATMENT", "TREATMENT", "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Legacy NHIN POU Treatment"));
-
-                // This is a case where the proper coded value is used. There are other parts of the SAML assertions
-                // that are tweaked to generate an error condition.
-                allCodes.put("REQUESTATTRS", new CodedValue("REQUESTATTRS", "REQUEST", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Individual Access Services"));
-                // Added 2023.12.13 to support QTF 1.1 negative testing
-                allCodes.put("T-IAS-ATTRS", new CodedValue("T-IAS-ATTRS", "T-IAS", "2.16.840.1.113883.3.7204.1.5.2.1", "RCE-purpose", "Individual Access Services"));
-                // End 2023.12.13
-
-                // Added 2024.09.28 to support eHx ACP
-                allCodes.put("ACP-Ernser",   new CodedValue("ACP-Ernser",   "COVERAGE", "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Disclosures for insurance or disability coverage determination"));
-                allCodes.put("ACP-Orn",      new CodedValue("ACP-Orn",      "COVERAGE", "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Disclosures for insurance or disability coverage determination"));
-                allCodes.put("ACP-Predovic", new CodedValue("ACP-Predovic", "COVERAGE", "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Disclosures for insurance or disability coverage determination"));
-                allCodes.put("ACP-Quigley",  new CodedValue("ACP-Quigley",  "COVERAGE", "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Disclosures for insurance or disability coverage determination"));
-                allCodes.put("ACP-Simonis",  new CodedValue("ACP-Simonis",  "COVERAGE", "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Disclosures for insurance or disability coverage determination"));
-                allCodes.put("ACP-West",     new CodedValue("ACP-West",     "COVERAGE", "2.16.840.1.113883.3.18.7.1", "nhin-purpose", "Disclosures for insurance or disability coverage determination"));
-                // End add 2024.09.28
-
- */
             }
         }
     }
